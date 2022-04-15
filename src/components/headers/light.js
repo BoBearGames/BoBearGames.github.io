003457ee632efca477a6099a3be8ff3646ef9935
BoBearGames <bobeargames@gmail.com>
2022-04-14 21:35:08 -0400

    fix mobile nav x and update colours

--- src/components/headers/light.js
+++ src/components/headers/light.js
@@ -29,7 +29,7 @@ export const NavLink = tw.a`
 
 export const AnchorNavLink = tw(AnchorLink)`text-lg my-2 lg:text-sm lg:mx-6 lg:my-0
 font-bold tracking-wide transition duration-300
-pb-1 border-b-2 border-transparent hover:border-primary-500 hocus:text-primary-500
+pb-1 border-b-2 border-transparent hover:border-blue-900 hocus:text-blue-900
 `;
 
 export const PrimaryLink = tw(NavLink)`
@@ -49,7 +49,7 @@ export const LogoLink = styled(NavLink)`
 
 export const MobileNavLinksContainer = tw.nav`flex flex-1 items-center justify-between`;
 export const NavToggle = tw.button`
-  lg:hidden z-20 focus:outline-none hocus:text-primary-500 transition duration-300
+  lg:hidden z-20 focus:outline-none hocus:text-blue-900 transition duration-300
 `;
 export const MobileNavLinks = motion(styled.div`
   ${tw`lg:hidden z-10 fixed top-0 inset-x-0 mx-4 my-6 p-8 border text-center rounded-lg text-gray-900 bg-white`}
@@ -85,7 +85,7 @@ export default ({ roundedHeaderButton = false, logoLink, links, className, colla
       <NavLink href="/#" tw="lg:ml-12!">
         Login
       </NavLink>
-      <PrimaryLink css={roundedHeaderButton && tw`rounded-full`}href="/#">Sign Up</PrimaryLink>
+      <PrimaryLink css={roundedHeaderButton && tw`rounded-full`} href="/#">Sign Up</PrimaryLink>
     </NavLinks>
   ];
 
@@ -113,9 +113,12 @@ export default ({ roundedHeaderButton = false, logoLink, links, className, colla
         {logoLink}
         <MobileNavLinks initial={{ x: "150%", display: "none" }} animate={animation} css={collapseBreakpointCss.mobileNavLinks}>
           {links}
+          <NavToggle onClick={toggleNavbar} className={showNavLinks ? "open" : "closed"}>
+            {showNavLinks && <CloseIcon tw="w-6 h-6" />}
+          </NavToggle>
         </MobileNavLinks>
         <NavToggle onClick={toggleNavbar} className={showNavLinks ? "open" : "closed"}>
-          {showNavLinks ? <CloseIcon tw="w-6 h-6" /> : <MenuIcon tw="w-6 h-6" />}
+          {!showNavLinks && <MenuIcon tw="w-6 h-6" />}
         </NavToggle>
       </MobileNavLinksContainer>
     </Header>
